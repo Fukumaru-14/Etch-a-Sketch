ed498d31ab43a08964604523f9e99321f5579360
Fukumaru <simon.c.arango.co@gmail.com>
2025-08-25 14:33:22 -0500

    Exercise for TOP done, now working on me version!

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-color = prompt('type, color')
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+
+
+
+let board = document.querySelector(".board")
 function createBoard (size) {
-    size = Number(window.prompt("How many pixels do you want your board to have?"));
+    
     if (size > 50) {
         alert("You can use more than 100px in this canvas!")
     } else {
-    let board = document.querySelector(".board")
-    console.log(board)
-   
-    sizeBoard = size * size;
-    console.log(size)
-    for (let i = 0; i < sizeBoard; i++) {
+        console.log(board)
+        
+        sizeBoard = size * size;
+        console.log(size)
+        for (let i = 0; i < sizeBoard; i++) {
         let divs = document.createElement("div");
         divs.style.backgroundColor = "#ffffffff";
         divs.style.flex = '1';
@@ -17,10 +28,17 @@ function createBoard (size) {
         divs.style.width = `calc(100% / ${size})`;
         divs.style.aspectRatio = '1 / 1'
         divs.addEventListener("mouseover", ()=> {
-        divs.style.backgroundColor = color}
-    );
+        divs.style.backgroundColor = getRandomColor()}            
+        );
         board.insertAdjacentElement("beforeend", divs);     
+    }
 }
 }
+createBoard(16);
+let renew = document.querySelector(".renew");
+renew.addEventListener("click", newBoard)
+function newBoard(size) {
+    size = Number(window.prompt("How many pixels do you want your board to have?"));
+    document.querySelector(".board").innerHTML = "";
+    createBoard(size)
 }
-createBoard();
