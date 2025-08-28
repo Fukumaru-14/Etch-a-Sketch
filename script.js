7439f64199c8ea8d9cda6431bc93d4614db11168
Fukumaru <simon.c.arango.co@gmail.com>
2025-08-27 22:14:38 -0500

    Finished personal version

--- script.js
+++ script.js
@@ -1,26 +1,65 @@
-color = prompt('type, color')
+function getColor() {
+    colorChoose = document.getElementById("colorPicker").value;
+    color = colorChoose;
+    return color
+}
+
+function getOpacity() {
+    opacityChoose = document.getElementById("opacityPicker").value;
+    opacity = opacityChoose;
+    return opacity
+}
+
+let click = false;
+
+let board = document.querySelector(".board")
 function createBoard (size) {
-    size = Number(window.prompt("How many pixels do you want your board to have?"));
-    if (size > 50) {
-        alert("You can use more than 100px in this canvas!")
-    } else {
-    let board = document.querySelector(".board")
-    console.log(board)
-   
-    sizeBoard = size * size;
-    console.log(size)
+    
+    if (size < 2 || size > 100 || size == NaN) {
+        alert("You can't use more than 100px in this canvas!");
+    }
+    else {
+        console.log(board)
+        sizeBoard = size * size;
+        console.log(size)
     for (let i = 0; i < sizeBoard; i++) {
         let divs = document.createElement("div");
+        divs.setAttribute("id", "divs");
         divs.style.backgroundColor = "#ffffffff";
         divs.style.flex = '1';
+        divs.style.opacity = 0.1;
         divs.style.height = `calc(100% / ${size})`;
         divs.style.width = `calc(100% / ${size})`;
         divs.style.aspectRatio = '1 / 1'
-        divs.addEventListener("mouseover", ()=> {
-        divs.style.backgroundColor = color}
-    );
         board.insertAdjacentElement("beforeend", divs);     
+        divs.addEventListener("mouseover", colorDiv)
+    } 
+} 
 }
+function colorDiv() {
+    if (click){
+        this.style.backgroundColor = getColor();
+        this.style.opacity = getOpacity();}
 }
+createBoard(16);
+let renew = document.querySelector(".renew");
+renew.addEventListener("click", newBoard)
+function newBoard(size) {
+    size = Number(window.prompt("How many pixels do you want your board to have?"));
+    document.querySelector(".board").innerHTML = "";
+    createBoard(size)
 }
-createBoard();
+document.querySelector('body').addEventListener('click', () => {
+    click = !click;
+}
+);
+
+let resetButton = document.getElementById("reset");
+
+resetButton.addEventListener("click", reset);
+function reset(){
+    let board = document.querySelector(".board");
+    let squares = board.querySelectorAll("div")
+    squares.forEach((div) => (div.style.backgroundColor = "white"));
+}
+
